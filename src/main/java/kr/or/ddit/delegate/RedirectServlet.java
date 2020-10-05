dd92a2806e302408740e418d9f4fb316b9a991df
2mh <2mh4000@gmail.com>
2020-10-05 09:06:56 +0900

    "maven  build cycle
    배포 자동화
    
    로깅 라이브러리
     . 로깅 레벨
     . 문자열 결합 주의"

--- src/main/java/kr/or/ddit/delegate/RedirectServlet.java
+++ src/main/java/kr/or/ddit/delegate/RedirectServlet.java
@@ -10,18 +10,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Servlet implementation class RedirectServlet
  */
 @WebServlet("/redirectServlet")
 public class RedirectServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	
+	//인자로 입력한 클래스의 패키지 정보를 확인 : kr.or.ddit.delegate.RedirectServlet
+	private static final Logger logger = LoggerFactory.getLogger(RedirectServlet.class);
+    
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//servlet은 응답을 만들어 내는 역할이 아니라 요청을 받고 로직을 처리한 후 
 		//jsp에게 화면 응답 생성을 위임한다.
 		
-		System.out.println("redirectServlet.doGet()");
+		//문자열 결합만 조심하자 "redirectServlet" + "doGet()"을 올바르지 않다. "redirectServlet {}", "doGet()"
+		logger.debug("redirectServlet.doGet()"+"test");
 		
 		//응답을 다른 jsp에게 위임하는 첫번째 방법 : redirect
 		//response 객체의 sendRedirect 메서드를 통해 클라이언트에게 재요청을 보낼 주소를 알려준다
--- src/main/webapp/delegate/redirectView.jsp
+++ src/main/webapp/delegate/redirectView.jsp
@@ -1,7 +1,6 @@
 <%@page import="java.util.List"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<% List<String> rangers =  (List<String>)request.getAttribute("rangers"); %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,6 +15,9 @@
 			<th>이름</th>
 		</tr>
 		<%--request객체에 저장된 rangers 속성을 이용하여 tr,td 그리고 ranger이름 출력 --%>
+		
+		<%if(request.getAttribute("rangers")!=null){	%>
+		<% List<String> rangers =  (List<String>)request.getAttribute("rangers"); %>
 		<%
 // 			for(int i=0;i<rangers.size();i++){
 			for(String ranger : rangers){
@@ -26,6 +28,10 @@
 			</tr>
 				
 		<%	}%>
+	   <%}%>  
+			<tr> 
+				<td>레인저가 없습니다.</td>
+			</tr>
 		
 		
 	</table>
