98804c3c43adb78acda4e4a2e5b830a0907ff578
2mh <2mh4000@gmail.com>
2020-10-20 09:15:10 +0900

    "Filter  . 전처리, 후처리"

--- src/main/java/kr/or/ddit/filter/RequestCounterFilter.java
+++ src/main/java/kr/or/ddit/filter/RequestCounterFilter.java
@@ -16,15 +16,16 @@ import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class RequestCounterFilter implements Filter{
+public class RequestCounterFilter implements Filter {
 	private static final Logger logger = LoggerFactory.getLogger(RequestCounterFilter.class);
 	Map<String, Integer> requestCounterMap;
+
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
 		logger.debug("RequestCounterFilter.init()");
 		requestCounterMap = new HashMap<String, Integer>();
 		// request, session, application
-		
+
 		ServletContext sc = filterConfig.getServletContext();
 		sc.setAttribute("requestCounterMap", requestCounterMap);
 	}
@@ -33,39 +34,46 @@ public class RequestCounterFilter implements Filter{
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		logger.debug("RequestCounterFilter.doFilter()");
-		
-		HttpServletRequest req = (HttpServletRequest)request;
-		logger.debug("uri :{}",req.getRequestURI());
-		
-		//uri별 요청 횟수
+
+		HttpServletRequest req = (HttpServletRequest) request;
+		logger.debug("uri :{}", req.getRequestURI());
+
+		// uri별 요청 횟수
 		// /memberList 12
 		// /jstl/jstl_fmt_date.jsp 20
-		//List, Map, Set
-		
+		// List, Map, Set
+
 //		map객체에서 uri에 해당하는 요청이 있었는지 확인
 //		없으면 값을 1로 해서 새로운 key로 추가
 //		있으면 기존값에서 1을 더해 값을 수정
-		
+
 		Integer value = requestCounterMap.get(req.getRequestURI());
-		//해당 uri로 최초요청
-		if(value == null) {
+		// 해당 uri로 최초요청
+		if (value == null) {
 			requestCounterMap.put(req.getRequestURI(), 1);
 		}
-		//해당 uri로 한번이상 요청이 존재했던 경우
+		// 해당 uri로 한번이상 요청이 존재했던 경우
 		else {
-			requestCounterMap.put(req.getRequestURI(), value+1);
+			requestCounterMap.put(req.getRequestURI(), value + 1);
 		}
+
+		// 어떤 자료구조를 쓰면 좋을까??
+
+		// 등록된 다른 필터로 요청 위임
+		// 만약 더이상 등록된 필터가 없을 경우 요청을 처리할 서블릿 / jsp로 요청을 전달
 		
-		//어떤 자료구조를 쓰면 좋을까??
+		logger.debug("RequestCounterFilter 전처리 부분-chain.doFilter 호출 전");
+		//전처리 : 요청이 서블릿으로 가기전에 실행되는 부분
+		chain.doFilter(request, response); //servlet 처리
 		
-		//등록된 다른 필터로 요청 위임
-		//만약 더이상 등록된 필터가 없을 경우 요청을 처리할 서블릿 / jsp로 요청을 전달
-		chain.doFilter(request, response);
+		logger.debug("RequestCounterFilter 전처리 부분-chain.doFilter 호출 후");
+		// 후처리 -> servlet에서 응답생성후 응답이 웹브라우저로 가는 단계에서 후속처리
+
 	}
 
 	@Override
 	public void destroy() {
-		
+
 	}
 
 }
